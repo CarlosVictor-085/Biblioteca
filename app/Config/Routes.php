1d1d8348539bc22348b63561241c72c2bb5fcb20
Victor <victor@gmail.com>
2024-11-22 10:28:13 -0300

    Att

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,7 +18,6 @@ $routes->post('/login/authenticate', 'Login::authenticate');
 
 $routes->get('/logout', 'Login::logout');
 
-$routes->set404Override('Erro::index');
 
 
 //$routes->setAutoRoute(true);
